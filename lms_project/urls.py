27042b445dea5d849ba5ba9d87471fae25c46b2d
unknown <kowsalya2025>
2026-02-12 17:41:03 +0530

    Add database dump for production

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -1,35 +1,22 @@
-"""
-URL configuration for lms_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    
-    # Remove this line - you're using Cloudinary, not ckeditor_uploader
-    # path('ckeditor/', include('ckeditor_uploader.urls')),
-    
-    # LMS app URLs
     path('', include('lms.urls')),
 ]
 
-# Serve media only in development
-if settings.DEBUG:
+# 🔴 CRITICAL FIX: Serve media files on Render
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT
+        }),
+    ]
+else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
